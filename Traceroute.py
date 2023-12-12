4cf9a553e0898000bf35c5df2e4f7d54d4492ea8
Kuangxun <1405567172@qq.com>
2023-12-12 23:22:47 +0800

    change at pycharm 12.12

--- Traceroute.py
+++ Traceroute.py
@@ -6,13 +6,14 @@ import socket
 from _socket import IPPROTO_IP, IP_TTL
 from socket import timeout
 
+sequence_number=1  # 设置一个初始序列号
 ICMP_ECHO_REQUEST = 8  # ICMP type code for echo request messages
 ICMP_ECHO_REPLY = 0  # ICMP type code for echo reply messages
 ICMP_Type_Unreachable = 3  # unacceptable host
 ICMP_Type_Overtime = 11  # request overtime
 MAX_HOPS = 30
 TIMEOUT = 3  # 设置了每个跳数的超时时间
-big_end_sequence = '!bbHh'
+big_end_sequence = '!bbHhh'
 TRIES =3  # 每一跳的尝试次数
 
 
@@ -37,18 +38,21 @@ def checksum(strings):
 
 
 def build_packet():
+    global sequence_number
+    identifier = os.getpid() & 0xFFFF  # Get the pid of the current process as the identifier
     icmp_checksum = 0
     # 1. Build ICMP header
-    icmp_header = struct.pack(big_end_sequence, ICMP_ECHO_REQUEST, 0, icmp_checksum, 1)
+    icmp_header = struct.pack(big_end_sequence, ICMP_ECHO_REQUEST, 0, icmp_checksum, identifier, sequence_number)
 
     time_send = struct.pack('!d', time.time())
 
     # 2. Checksum ICMP packet using given function
     icmp_checksum = checksum(icmp_header + time_send)
     # 3. Insert checksum into packet
-    icmp_header = struct.pack(big_end_sequence, ICMP_ECHO_REQUEST, 0, icmp_checksum, 1)
+    icmp_header = struct.pack(big_end_sequence, ICMP_ECHO_REQUEST, 0, icmp_checksum, identifier, sequence_number)
     # 4. Send packet using socket
     icmp_packet = icmp_header + time_send
+    sequence_number += 1
     return icmp_packet
 
 
@@ -72,7 +76,7 @@ def get_route(hostname):
                 icmp_socket.sendto(build_packet(), (destination_ip, 0))
                 t = time.time()
                 time_begin_receive = time.time()
-                if_got = select.select([icmp_socket], [], [], TIMEOUT)
+                if_got = select.select([icmp_socket], [], [], TIMEOUT)  # 检测是否收到报文
                 time_during_receive = time.time() - time_begin_receive
                 if not if_got[0]:
                     print("  %d     *        Request timed out." % ttl)
@@ -85,12 +89,11 @@ def get_route(hostname):
             except timeout:
                 continue
             else:
-                byte_in_double = struct.calcsize("!d")
-                time_sent = struct.unpack("!d", rec_packet[26: 26 + byte_in_double])[0]
-                rec_header = rec_packet[20:26]
-                types, _, _, _ = struct.unpack(big_end_sequence, rec_header)
-
-                result = (ttl, time_received - t * 1000, addr[0])
+                byte_in_double = struct.calcsize("!d")  # 确定double在网络字节序下所占的字节数
+                time_sent = struct.unpack("!d", rec_packet[28: 28 + byte_in_double])[0]
+                rec_header = rec_packet[20:28]
+                types, _, _, _, _ = struct.unpack(big_end_sequence, rec_header)
+                result = (ttl, time_received - t * 1000, addr[0])  # 元组第一个元素是ip地址，第二个是端口
                 results.append(result)
 
                 if types == 11:
@@ -105,8 +108,19 @@ def get_route(hostname):
                 continue
             finally:
                 icmp_socket.close()
-
+# 检查输入是否有效
+def validate_input(input_str):
+    try:
+        socket.getaddrinfo(input_str, None)
+        return True
+    except socket.gaierror:
+        return False
 
 if __name__ == '__main__':
-    user_input = input("Please input <host or ip>")
+    while True:
+        user_input = input(f"Please input <host or ip>")
+        if validate_input(user_input):
+            break
+        else:
+            print(f"Invalid input. Please enter a valid host name or IP address")
     get_route(user_input)
